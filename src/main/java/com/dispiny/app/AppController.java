2e6ae216946cf41430e7ac2fa7c292b1f1042977
Uijin Lee <100072451+dml113@users.noreply.github.com>
2024-11-03 19:25:33 +0900

    Update AppController.java

--- src/main/java/com/dispiny/app/AppController.java
+++ src/main/java/com/dispiny/app/AppController.java
@@ -8,7 +8,7 @@ public class AppController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, This is Dispiny Grace Application!";
+        return "version 1.0.0";
     }
 
     @GetMapping("/long-running")
