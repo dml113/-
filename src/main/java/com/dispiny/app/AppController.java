62da8ea687dfe983074fa57af4a75197944e6e34
Uijin Lee <100072451+dml113@users.noreply.github.com>
2024-11-04 08:22:42 +0900

    Update AppController.java

--- src/main/java/com/dispiny/app/AppController.java
+++ src/main/java/com/dispiny/app/AppController.java
@@ -8,7 +8,7 @@ public class AppController {
 
     @GetMapping("/")
     public String hello() {
-        return "version 5.0.0";
+        return "version 6.0.0";
     }
 
     @GetMapping("/long-running")
